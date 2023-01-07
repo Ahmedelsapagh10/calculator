0adc2108d48c673e81a0da0f5450a970950f9b54
Ahmed Elsapagh <ahmedelsapagh10@gmail.com>
2023-01-07 22:39:16 +0200

    first commit

---
+++ lib/view/homeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:drawing/view/CanvasView.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DrawingScreen(),
+    );
+  }
+}
